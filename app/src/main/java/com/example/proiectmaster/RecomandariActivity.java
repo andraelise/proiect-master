341bf0fb5b74a05e4d80d377e6c7cc72d82f5cd3
Andra Seserman <andra.seserman@nokia.com>
2021-04-20 00:28:05 +0300

    UI pentru recomandari.

--- app/src/main/java/com/example/proiectmaster/RecomandariActivity.java
+++ app/src/main/java/com/example/proiectmaster/RecomandariActivity.java
@@ -1,20 +1,18 @@
 package com.example.proiectmaster;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.proiectmaster.Models.Recomandare;
-import com.example.proiectmaster.Utils.RecomandariAdapter;
 
 import java.util.ArrayList;
 
 public class RecomandariActivity extends AppCompatActivity {
-//    private ListView listView;
-//    private RecomandariAdapter recomandariAdapter;
-//    private ArrayList<Recomandare> recomandariList = new ArrayList<>();
+    private ListView listView;
+    private RecomandariAdapter recomandariAdapter;
+    private ArrayList<Recomandare> recomandariList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +20,18 @@ public class RecomandariActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recomandari);
         getSupportActionBar().setTitle("Recomandari");
 
-//        listView = findViewById(R.id.recomandariListView);
-//        displayList();
+        listView = findViewById(R.id.recomandariListView);
+        displayList();
     }
 
-//    private void displayList() {
-//        recomandariList.add(new Recomandare("Bicicleta", 20, 3, "Pastrati pulsul sub 90."));
-//        recomandariList.add(new Recomandare("Mers", 30, 4, "Plimbati-va 2 kilometri."));
-//        recomandariList.add(new Recomandare("Alergat", 10, 1, "Alergati in ritm usor."));
-//
-//        RecomandariAdapter = new RecomandariAdapter(this, recomandariList);
-//        listView.setAdapter((ListAdapter) recomandariList);
-//    }
+    private void displayList() {
+        recomandariList.add(new Recomandare("Bicicleta", 20, 3, "Pastrati pulsul sub 90."));
+        recomandariList.add(new Recomandare("Mers", 30, 4, "Plimbati-va 2 kilometri."));
+        recomandariList.add(new Recomandare("Alergat", 10, 1, "Alergati in ritm usor."));
+        recomandariList.add(new Recomandare("Plimbare", 10, 1, "Mergeti in ritm usor."));
+        recomandariList.add(new Recomandare("Bicicleta", 20, 3, "Pastrati pulsul sub 90."));
+
+        recomandariAdapter = new RecomandariAdapter(this, recomandariList);
+        listView.setAdapter(recomandariAdapter);
+    }
 }
